3b5db651ba1b16f4332a18a9af20653b2f93dd1c
silvyanarisma <silvyana.risma@gmail.com>
2021-09-29 00:40:12 +0700

    Jurnal01.java

---
+++ Jurnal01.java
@@ -0,0 +1,10 @@
+public class Jurnal01 {
+	
+	public static void main(String[] args) {
+	String number = "6706210074";
+	String kelas = "45-01";
+	System.out.println("Nama: " + "Silvyana Dyah Rismawati");
+	System.out.println("NIM: " + number);
+	System.out.println("Kelas: " + kelas);
+	}
+}
